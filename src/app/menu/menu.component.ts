edfd86e9338dec46b1a50cf7f2261e106a03c97c
Chinnam-JanakiDevi <chinnam.janakidevi123@gmail.com>
2023-09-28 22:15:14 +0530

    sih

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  cseshow=false;
+  showmenu=true
+  showcse(){
+    this.cseshow=true;
+    this.showmenu=false;
+  }
 }
